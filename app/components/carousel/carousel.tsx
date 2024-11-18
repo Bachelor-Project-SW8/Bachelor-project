f91fc260c2a0d1ec5e8618714e6181cd0d77a06d
Mwaldemar <304837@via.dk>
2024-11-18 15:16:37 +0100

    carousel for project

--- app/components/carousel/carousel.tsx
+++ app/components/carousel/carousel.tsx
@@ -1,82 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import styles from './carousel.module.scss';
-import Image from 'next/image';
+import { useState } from "react"
+import { ChevronLeft, ChevronRight, Circle, CircleDot } from "lucide-react"
+import styles from './carousel.module.scss'
+import clsx from "clsx"
 
-interface Product {
-  id: number;
-  title: string;
-  description: string;
-  image: string;
+type CarouselProps = {
+    imageUrls: string[]
 }
 
-const ProductCarousel: React.FC = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [activeIndex, setActiveIndex] = useState(0);
+export function Carousel({ imageUrls }: CarouselProps) {
+    const [imageIndex, setImageIndex] = useState(0)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get<Product[]>('https://fakestoreapi.com/products');
-        setProducts(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+    function showNextImage() {
+        setImageIndex(index => {
+            if (index === imageUrls.length - 1) return 0
+            return index + 1
+        })
+    }
 
-    fetchData();
-  }, []);
+    function showPrevImage() {
+        setImageIndex(index => {
+            if (index === 0) return imageUrls.length - 1
+            return index - 1
+        })
+    }
 
-  const handleIndicatorClick = (index: number) => {
-    setActiveIndex(index);
-  };
-
-  const handlePrevClick = () => {
-    setActiveIndex((prevIndex) => (prevIndex === 0 ? products.length - 1 : prevIndex - 1));
-  };
-
-  const handleNextClick = () => {
-    setActiveIndex((prevIndex) => (prevIndex === products.length - 1 ? 0 : prevIndex + 1));
-  };
-
-  const imageWidth = 300;
-
-  const activeItemPosition = {
-    transform: `translateX(-${activeIndex * imageWidth}px) translateX(calc((100% - ${imageWidth}px) / 2))`,
-  };
-
-  return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>Product Carousel</h2>
-      <div id="carouselExampleIndicators" className={styles.carousel} data-ride="carousel">
-        <ol className={styles.indicators}>
-          {products.map((product, index) => (
-            <li
-              key={index}
-              onClick={() => handleIndicatorClick(index)}
-              className={`${styles.indicator} ${index === activeIndex ? styles.active : ''}`}
-            ></li>
-          ))}
-        </ol>
-        <div className={styles.inner} style={activeItemPosition}>
-          {products.map((product, index) => (
-            <div key={index} className={styles.slide}>
-              <Image className={styles.image} src={product.image} alt={product.title} width={250} height={250} />
-              <div className={styles.caption}>
-                <h5 className={styles.title}>{product.title}</h5>
-              </div>
+    return (
+        <div className={styles.carouselSlider}>
+            <div className={styles.carouselImages}>
+                {imageUrls.map(url => (
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img
+                        alt="images"
+                        key={url}
+                        src={url}
+                        className={styles.sliderImg}
+                        style={{ translate: `${-100 * imageIndex}%` }} />
+                ))}
             </div>
-          ))}
+            <button onClick={showPrevImage} className={clsx(styles.imgSliderBtn, styles.left)}>
+                <ChevronLeft />
+            </button>
+            <button onClick={showNextImage} className={clsx(styles.imgSliderBtn, styles.right)}>
+                <ChevronRight />
+            </button>
+            <div className={styles.carouselDotBtn}>
+                {imageUrls.map((_, index) => (
+                    <button
+                        key={index}
+                        className={styles.dotBtn}
+                        onClick={() => setImageIndex(index)}
+                    >
+                        {index === imageIndex ? <CircleDot /> : <Circle />}
+                    </button>
+                ))}
+            </div>
         </div>
-        <button className={styles.controlPrev} onClick={handlePrevClick}>
-          Prev
-        </button>
-        <button className={styles.controlNext} onClick={handleNextClick}>
-          Next
-        </button>
-      </div>
-    </div>
-  );
-};
-
-export default ProductCarousel;
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 'use client'
-import ProductCarousel from "./components/carousel/carousel";
+
+import pic1 from "../images/pic1.jpg"
+import pic2 from "../images/pic2.jpg"
+import pic3 from "../images/pic3.jpg"
+import pic4 from "../images/pic4.jpg"
+import pic5 from "../images/pic5.jpg"
+
+import { Carousel } from "./components/carousel/carousel"
+import styles from './page.module.scss'
+
+const IMAGES = [pic1, pic2, pic3, pic4, pic5]
 
 export default function Home() {
+
+  const imageUrls = IMAGES.map((image) => image.src); // Convert to string[]
+
   return (
-    <div>
-      <ProductCarousel />
+    <div className={styles.carousel}>
+      <Carousel imageUrls={imageUrls} />
     </div>
   );
 }
