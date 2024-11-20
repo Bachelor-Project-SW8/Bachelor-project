93a3c8cac9af54b88c31b43d69a8fb26d1b01c4f
Mwaldemar <304837@via.dk>
2024-11-20 14:11:55 +0100

    Creation of widgets and product tiles

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,12 @@ import pic3 from "../images/pic3.jpg"
 import pic4 from "../images/pic4.jpg"
 import pic5 from "../images/pic5.jpg"
 
+import steelBracelet from "./components/widget/widgetPictures/steelbracelet.png"
+
 import { Carousel } from "./components/carousel/carousel"
+import { Widget } from "./components/widget/widget"
 import styles from './page.module.scss'
+import { ProductTile } from "./components/productTile/productTile"
 
 const IMAGES = [pic1, pic2, pic3, pic4, pic5]
 
@@ -16,8 +20,22 @@ export default function Home() {
   const imageUrls = IMAGES.map((image) => image.src); // Convert to string[]
 
   return (
-    <div className={styles.carousel}>
-      <Carousel imageUrls={imageUrls} />
+    <div>
+      <div className={styles.carousel}>
+        <Carousel imageUrls={imageUrls} />
+      </div>
+      <Widget
+        className={styles.widgetPicture}
+        picture={"https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/bltfecf8e7ec7041c24/660e6fd0231cf0c3a2ccb798/steelcufffnew_steel_focus_-_drop2684_silver.jpg?format=pjpg&auto=webp&quality=75%2C90&width=640"}
+        text="Steel Bracelets"
+      />
+      <ProductTile
+        className={styles.productTile}
+        picture="https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/bltc89eabc7ff6f8555/60dbd0135775160f9f22e567/s-br-10mm_1_3.jpg?format=pjpg&auto=webp&quality=75%2C90&width=1080"
+        productName="Lolle"
+        price={123}
+        brand="Lolle123"
+      />
     </div>
   );
 }
