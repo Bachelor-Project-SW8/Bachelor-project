9963015c0907bba72967c98614ede43423c25701
OliverJohannesen <Oliverjohannesen@gmail.com>
2024-11-27 12:10:56 +0100

    product data

--- Trendhim.sql
+++ Trendhim.sql
@@ -52,3 +52,23 @@ Låsen der holder kædeleddene sammen er nem at åbn og lukke.","https://eu-imag
 --     FOREIGN KEY (ProductID) REFERENCES Product(ProductID) ON DELETE CASCADE,
 --     FOREIGN KEY (ColorID) REFERENCES Color(ColorID) ON DELETE CASCADE
 -- );
+
+INSERT INTO Category (CategoryName, CategoryPicture) 
+VALUES 
+("Stainless Steel", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/blt172e6b362ac87f8b/60dbcb88892a730f58849cb1/jwm-megashoot-bracelets-15112.1.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384"),
+("Leather", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/blt12180f016190c61e/60dbbd6f993b043846436950/11606_square.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384"),
+("Beaded Bracelets", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/bltc60d9c155d9a9631/60dba52415da443b102d081b/2232.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384");
+
+
+INSERT INTO Product (ProductName, Price, Description, ProductPicture) 
+VALUES 
+("Classic Stainless Steel Bracelet", 299.00, "Et klassisk armbånd i rustfrit stål, perfekt til hverdagsbrug.", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/blt9fd45a78e6041922/62d7e3ae652a953c7dc54210/15108-s-brac1.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384"),
+("Leather Strap Bracelet", 149.00, "Et armbånd med en læderrem og rustfri stålspænde, perfekt til den afslappede stil.", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/blt4f21efff1c790e7e/60dba2da467112384c7b46d8/black-on-black-bracelet-set.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384"),
+("Gold Plated Chain Bracelet", 799.00, "Et elegant armbånd med guldbelægning, ideelt til festlige lejligheder.", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/bltebaf8a116bc62369/62d7e9666c31723d18906afb/15096-g-brac1.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384"),
+("Silver Twist Bracelet", 599.00, "Et flot og stilfuldt armbånd med et snoet design i sterlingsølv.", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/blt28e057d2285a1864/60dc03f8993b04384644646c/9-1_14_2_324_1_45.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384"),
+("Black Rubber & Steel Bracelet", 199.00, "Moderne armbånd med gummi og stål for et råt og urban look.", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/blt454db6faf33a6a7e/60dc10572e95e10f21f2c2a4/20255.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384"),
+("Steel Sport Bracelet", 399.00, "Et let og holdbart sport armbånd i titanium, designet til aktive personer.", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/blt0ac38c8a61e66fd4/60dc20742e95e10f21f2fcce/brusd45a14861196b84229d5351457fc202f_2.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384"),
+("Beaded Charm Bracelet", 129.00, "Et smukt armbånd med perler og charm, perfekt som gave.", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/blt141816de075d7712/60dbb8dfa7307e39e4df1f7f/5-2_297.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384"),
+("Rope Bracelet with Silver Clasp", 249.00, "Et smukt, velegnet armbånd med rebdesign og en sølvklips.", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/blt50fe44853c538ed7/63624fee2109c24a1437906e/11-zeismont-ll-collins-leather-bracelets8103-black-kolin_3.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384"),
+("Minimalist Black Steel Bracelet", 399.00, "Et minimalistisk armbånd i sort stål, ideelt til både mænd og kvinder.", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/blt8626dd8c4ffa0ea3/62d7ea844473103b37954c65/15083-b-brac1.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384"),
+("Gold Mesh Bracelet", 899.00, "Et luksuriøst armbånd i guldnet, perfekt til en elegant aften.", "https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/bltbf2306e6ec7c2ac6/60dbba64a37ada0f2ccfb591/g-br-10mm_1_3.jpg?format=pjpg&auto=webp&quality=75%2C90&width=384");
