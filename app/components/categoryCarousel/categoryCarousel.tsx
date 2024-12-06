6f0a06ea458f066bd2088282032eb886977d00af
Mwaldemar <304837@via.dk>
2024-12-06 11:15:48 +0100

    Fixed repsonsive issues on front page

--- app/components/categoryCarousel/categoryCarousel.tsx
+++ app/components/categoryCarousel/categoryCarousel.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 // npm install react-multi-carousel --save
 import Carousel from "react-multi-carousel";
@@ -15,15 +15,15 @@ export function CategoryCarousel({ children, className }: ProductCarouselProps) 
     const responsive = {
         largeDesktop: {
             breakpoint: { max: 4000, min: 3000 },
-            items: 2,
+            items: 5,
         },
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 3,
+            items: 4,
         },
         tablet: {
             breakpoint: { max: 1024, min: 650 },
-            items: 2,
+            items: 3,
         },
         mobile: {
             breakpoint: { max: 650, min: 0 },
@@ -38,6 +38,8 @@ export function CategoryCarousel({ children, className }: ProductCarouselProps) 
             responsive={responsive}
             draggable={true}
             swipeable={true}
+            infinite={false}
+            partialVisbile={false}
         >
             {children}
         </Carousel>
