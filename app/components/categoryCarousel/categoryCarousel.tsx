69db96d0b2c41aa1e8460456cfe17eee6b91a753
Mwaldemar <304837@via.dk>
2024-12-06 01:16:23 +0100

    Frontpage in progress + minor changes

---
+++ app/components/categoryCarousel/categoryCarousel.tsx
@@ -0,0 +1,45 @@
+import { ReactNode } from "react";
+
+// npm install react-multi-carousel --save
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import styles from './categoryCarousel.module.scss'
+import clsx from "clsx";
+
+type ProductCarouselProps = {
+    children: ReactNode;
+    className?: string;
+};
+
+export function CategoryCarousel({ children, className }: ProductCarouselProps) {
+    const responsive = {
+        largeDesktop: {
+            breakpoint: { max: 4000, min: 3000 },
+            items: 2,
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 650 },
+            items: 2,
+        },
+        mobile: {
+            breakpoint: { max: 650, min: 0 },
+            items: 2,
+        },
+    };
+
+    return (
+
+        <Carousel
+            className={clsx(className, styles.categoryCarouselContainer)}
+            responsive={responsive}
+            draggable={true}
+            swipeable={true}
+        >
+            {children}
+        </Carousel>
+    );
+}
