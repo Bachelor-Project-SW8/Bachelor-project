304ab751b1d6189a630d1bf5522c90437aad4086
Mwaldemar <304837@via.dk>
2024-12-08 19:53:17 +0100

    Product tiles i carousel på product page

--- app/components/productCarousel/productCarousel.tsx
+++ app/components/productCarousel/productCarousel.tsx
@@ -18,21 +18,35 @@ export function ProductCarousel({
   className,
 }: ProductCarouselProps) {
   const responsive = {
-    largeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 4,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
+    superLargeDesktop: {
+      breakpoint: { max: 3000, min: 2600 },
       items: 11,
       slidesToSlide: 3,
     },
+    largeDesktop: {
+      breakpoint: { max: 2600, min: 2150 },
+      items: 9,
+      slidesToSlide: 3,
+    },
+    desktop: {
+      breakpoint: { max: 2150, min: 1650 },
+      items: 7,
+      slidesToSlide: 3,
+    },
+    smallerDesktop: {
+      breakpoint: { max: 1650, min: 1200 },
+      items: 5,
+    },
+    smallDesktop: {
+      breakpoint: { max: 1200, min: 980 },
+      items: 4,
+    },
     tablet: {
-      breakpoint: { max: 1024, min: 600 },
-      items: 2,
+      breakpoint: { max: 980, min: 768 },
+      items: 3,
     },
     mobile: {
-      breakpoint: { max: 600, min: 0 },
+      breakpoint: { max: 768, min: 0 },
       items: 2,
     },
   };
